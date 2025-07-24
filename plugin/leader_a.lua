c1ce79cc2ab66a67b1df5924b09b25a3ea1a84e0
peter-lyr <llydrp.ldp@gmail.com>
2025-07-24 16:02:28 +0800

    <leader>ae create_file_under_cur

--- plugin/leader_a.lua
+++ plugin/leader_a.lua
@@ -4,7 +4,9 @@ require 'which-key'.register {
   ['<leader>a'] = { name = 'add', },
   ['<leader>a<leader>'] = { name = 'add.more', },
 
-  ['<leader>ai'] = { function() F.create_file_under_cur() end, 'create_file_under_cur', mode = { 'n', 'v', }, },
-  ['<leader>a<leader>i'] = { function() F.create_file_under_cwd() end, 'create_file_under_cwd', mode = { 'n', 'v', }, },
+  ['<leader>ae'] = { function() F.create_file_under_cur() end, 'create_file_under_cur', mode = { 'n', 'v', }, },
+  ['<leader>a<leader>e'] = { function() F.create_file_under_cwd() end, 'create_file_under_cwd', mode = { 'n', 'v', }, },
+  ['<leader>ai'] = { function() F.create_file_under_cur_with_line() end, 'create_file_under_cur_with_line', mode = { 'n', 'v', }, },
+  ['<leader>a<leader>i'] = { function() F.create_file_under_cwd_with_line() end, 'create_file_under_cwd_with_line', mode = { 'n', 'v', }, },
   ['<leader>ad'] = { function() F.mkdir_cur_tail() end, 'mkdir_cur_tail', mode = { 'n', 'v', }, },
 }
