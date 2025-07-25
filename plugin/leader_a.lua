5207f3cd871f311e9fdadb6a258d0aff520d3aa0
peter-lyr <llydrp.ldp@gmail.com>
2025-07-25 14:32:53 +0800

    <leader>ac F.create_index_file()

--- plugin/leader_a.lua
+++ plugin/leader_a.lua
@@ -9,4 +9,6 @@ require 'which-key'.register {
   ['<leader>ai'] = { function() F.create_file_under_cur_with_line() end, 'create_file_under_cur_with_line', mode = { 'n', 'v', }, },
   ['<leader>a<leader>i'] = { function() F.create_file_under_cwd_with_line() end, 'create_file_under_cwd_with_line', mode = { 'n', 'v', }, },
   ['<leader>ad'] = { function() F.mkdir_cur_tail() end, 'mkdir_cur_tail', mode = { 'n', 'v', }, },
+  ['<leader>ac'] = { function() F.create_index_file() end, 'create_index_file', mode = { 'n', 'v', }, },
+  ['<leader>ao'] = { function() F.sel_open_same_index_file() end, 'sel_open_same_index_file', mode = { 'n', 'v', }, },
 }
